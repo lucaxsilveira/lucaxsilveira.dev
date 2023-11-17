8a95531b23c913465ef33b99c3093c9998fd7814
lucaxsilveira <lucaxsilveira@gmail.com>
2023-11-17 14:15:23 -0300

    feat: prettier

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,16 @@
-import client, { sanityFetch } from "@/services/sanity";
-import { Post } from "@/types/post";
-import { getPosts } from "@/useCases/posts/get-posts";
+import client, { sanityFetch } from '@/services/sanity';
+import { Post } from '@/types/post';
+import { getPosts } from '@/useCases/posts/get-posts';
 
 const Home = ({ posts }: { posts: Post[] }) => {
-    return <pre>{JSON.stringify(posts, null, 2)}</pre>;
+  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
 };
 
 export async function getStaticProps() {
-    const posts = await getPosts();
-    return {
-        props: { posts },
-    };
+  const posts = await getPosts();
+  return {
+    props: { posts },
+  };
 }
 
 export default Home;
--- src/types/post.d.ts
+++ src/types/post.d.ts
@@ -1,18 +1,18 @@
 export interface Post {
-    _id: string;
-    title?: string;
+  _id: string;
+  title?: string;
+  slug?: string;
+  mainImage?: {
+    asset: {
+      url: string;
+    };
+  };
+  publishedAt?: string;
+  body?: any;
+  author?: {
+    name?: string;
+    photo?: string;
+    bio?: string;
     slug?: string;
-    mainImage?: {
-        asset: {
-            url: string;
-        };
-    };
-    publishedAt?: string;
-    body?: any;
-    author?: {
-        name?: string;
-        photo?: string;
-        bio?: string;
-        slug?: string;
-    };
+  };
 }
