97cf3e6837682ba193fafbe6c961b1c8b95dc8bb
lucas.silveira <lucas.silveira@g.globo>
2023-11-17 14:08:45 -0300

    feat: add sanity cms

---
+++ src/pages/home.tsx
@@ -0,0 +1,23 @@
+import client, { sanityFetch } from "@/services/sanity"
+import { Post } from "@/types/post"
+import { getPosts } from "@/useCases/posts/get-posts"
+
+
+const Home = ({ posts }: { posts: Post[]}) => {
+    return <pre>
+      {JSON.stringify(posts, null, 2)}
+    </pre>
+}
+
+
+
+
+export async function getStaticProps() {
+  const posts = await getPosts()
+  return {
+    props: { posts }
+  }
+}
+
+export default Home
+
