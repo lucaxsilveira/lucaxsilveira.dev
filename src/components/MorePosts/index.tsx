040c1cd6d70a864efef9000c361c6383dbd97950
lucaxsilveira <lucaxsilveira@gmail.com>
2023-12-13 09:35:43 -0300

    feat: filters to sanityFetch

--- src/components/MorePosts/index.tsx
+++ src/components/MorePosts/index.tsx
@@ -4,10 +4,24 @@ import { truncate } from 'lodash';
 import { IPost } from '@/types/post';
 import { getPosts } from '@/useCases/posts/get-posts';
 
+import { headers } from 'next/headers';
 import Image from '../Image';
 
 const MorePosts = async () => {
-  const posts: IPost[] = await getPosts({ page: 0, perPage: 4 });
+  const headersList = headers();
+  const pathname = headersList.get('x-pathname')?.replace('/posts/', '');
+
+  const posts: IPost[] = await getPosts({
+    page: 0,
+    perPage: 4,
+    filters: [
+      {
+        field: 'slug.current',
+        value: pathname,
+        operator: '!=',
+      },
+    ],
+  });
 
   const getDescription = (body: any) => {
     const bodyText = toPlainText(body);
@@ -31,34 +45,33 @@ const MorePosts = async () => {
         <p className="mb-4 text-sm ">Você também pode gostar de:</p>
         <div className="grid grid-cols-2 gap-8">
           {posts.map((post) => (
-            <div
-              className="post-bullet flex w-full flex-col"
-              key={post.slug.current}
-            >
-              <figure className="h-[200px] w-full">
-                <Image.Image
-                  className={'h-[200px] w-full object-cover object-center'}
-                  value={post.mainImage}
-                  isInline={false}
-                />
-              </figure>
-              <div className="mt-4">
-                <p className="text-md font-semibold">{post.title}</p>
-                <p className="mt-2 text-sm font-light text-gray-600">
-                  {post.subtitle
-                    ? getSubtitle(post.subtitle)
-                    : getDescription(post.body)}
-                </p>
+            <a href={`/posts/${post.slug.current}`} key={post.slug.current}>
+              <div className="post-bullet flex w-full flex-col">
+                <figure className="h-[200px] w-full">
+                  <Image.Image
+                    className={'h-[200px] w-full object-cover object-center'}
+                    value={post.mainImage}
+                    isInline={false}
+                  />
+                </figure>
+                <div className="mt-4">
+                  <p className="text-md font-semibold">{post.title}</p>
+                  <p className="mt-2 text-sm font-light text-gray-600">
+                    {post.subtitle
+                      ? getSubtitle(post.subtitle)
+                      : getDescription(post.body)}
+                  </p>
+                </div>
+                <div className="text-light mt-auto flex gap-2 pt-3 text-xs text-gray-400">
+                  {post.date && (
+                    <span className="after:top-0.3 after:relative after:ml-2 after:text-gray-500 after:content-['·'] ">
+                      {post.date}
+                    </span>
+                  )}
+                  <span>{post.readingTime && post.readingTime}</span>
+                </div>
               </div>
-              <div className="text-light mt-auto flex gap-2 pt-3 text-xs text-gray-400">
-                {post.date && (
-                  <span className="after:top-0.3 after:relative after:ml-2 after:text-gray-500 after:content-['·'] ">
-                    {post.date}
-                  </span>
-                )}
-                <span>{post.readingTime && post.readingTime}</span>
-              </div>
-            </div>
+            </a>
           ))}
         </div>
       </div>
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -4,3 +4,26 @@ export const estimateReadingTime = (text: string): number => {
   const readingTime = Math.ceil(numberOfWords / wordsPerMinute);
   return readingTime || 1;
 };
+
+interface IFilters {
+  [key: string]: {
+    field: string;
+    value: string;
+    operator: string;
+  };
+}
+
+export const buildFilterString = (filters: IFilters[]) => {
+  if (!Array.isArray(filters) || filters.length === 0) {
+    return '';
+  }
+
+  const filterStrings = filters.map((filter) => {
+    const { field, value, operator } = filter;
+    if (field && value && operator) {
+      return `${field} ${operator} ${JSON.stringify(value)}`;
+    }
+  });
+
+  return filterStrings.join(' && ');
+};
